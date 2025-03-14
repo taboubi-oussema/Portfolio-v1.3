763c4e9636deba17ceeba6448ca87388e8497078
unknown <taboubioussema78@gmail.com>
2025-03-14 17:52:37 +0100

    update

--- work/work.component.ts
+++ work/work.component.ts
@@ -19,6 +19,7 @@ export class WorkComponent implements OnInit {
       technologies: [
         "nodemon",
         "bcryptjs",
+        
         "dotenv",
         "express",
         "express-async-handler",
