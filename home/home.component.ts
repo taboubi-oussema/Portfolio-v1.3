db224b46ccdf20e9a8e2fe78753f4c4a858694db
unknown <taboubioussema78@gmail.com>
2025-03-21 14:38:26 +0100

    update

--- home/home.component.ts
+++ home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import lottie from 'lottie-web';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit() {
+    const container = document.getElementById('lottie');
+    if (container) {
+      lottie.loadAnimation({
+        container: container, // the DOM element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/home_1.json', // the path to the animation json
+      });
+    }
+  }
 
 }
--- not-found/not-found.component.ts
+++ not-found/not-found.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import lottie from 'lottie-web';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit{
+  ngOnInit() {
+    const container = document.getElementById('lottie');
+    if (container) {
+      lottie.loadAnimation({
+        container: container, // the DOM element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/404.json', // the path to the animation json
+      });
+    }
+  }
 
 }
